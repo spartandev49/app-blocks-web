499d1e0f58706cbd0df754e462c098aca99baa69
spartandev49 <spartan.dev49@gmail.com>
2026-09-01 13:43:27 -0500

    Expose combinatorial compiler and catalog APIs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
-export { CATALOG, compactCatalog, getBlock, getCatalog } from "./catalog.js";
-export { buildFile, compile, normalizeBase, writeBuild } from "./compiler.js";
+export {
+  ATTRIBUTE_ALIASES,
+  BLOCK_ALIASES,
+  CATALOG,
+  DESIGN_AXES,
+  RECIPE_COUNT,
+  VIRTUAL_BLOCK_COUNT,
+  VIRTUAL_BLOCKS_PER_FAMILY,
+  compactCatalog,
+  designManifest,
+  getBlock,
+  getCatalog,
+  normalizeCompactSource,
+  resolveDesign,
+  resolveFontPair,
+  resolvePalette,
+  resolveRecipe,
+  resolveVirtualBlock
+} from "./combinatorial-engine.js";
+export { ADVANCED_CSS, ADVANCED_RUNTIME, buildDesignCss } from "./combinatorial-assets.js";
+export { buildFile, compile, normalizeBase, writeBuild } from "./compiler-v2.js";
 export { AppBlocksError, formatDiagnostic, formatDiagnostics } from "./diagnostics.js";
-export { parse } from "./parser.js";
+export { parse } from "./parser-v2.js";
 export { renderNode, renderPage } from "./render.js";
 export { assertValid, validate } from "./validate.js";
 export { VERSION } from "./version.js";
