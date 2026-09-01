fb1ff7d2cfff1dba9dcefe05a818a7947f7e2f77
spartandev49 <spartan.dev49@gmail.com>
2026-09-01 14:03:40 -0500

    Expose conditional combinatorial compiler APIs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
-export { CATALOG, compactCatalog, getBlock, getCatalog } from "./catalog.js";
-export { buildFile, compile, normalizeBase, writeBuild } from "./compiler.js";
+export {
+  ATTRIBUTE_ALIASES,
+  BLOCK_ALIASES,
+  CATALOG,
+  DESIGN_AXES,
+  DENSITIES,
+  EXTENDED_CATALOG,
+  MACROS,
+  MOTIONS,
+  RECIPE_COUNT,
+  SEMANTIC_CATALOG,
+  SEMANTIC_MACRO_COUNT,
+  SHADOWS,
+  SHAPES,
+  SURFACES,
+  VIRTUAL_BLOCK_COUNT,
+  VIRTUAL_BLOCKS_PER_FAMILY,
+  compactCatalog,
+  designManifest,
+  getBlock,
+  getCatalog,
+  resolveDesign,
+  resolveFontPair,
+  resolvePalette,
+  resolveRecipe,
+  resolveVirtualBlock
+} from "./generation2.js";
+export { normalizeCompactSource } from "./normalizer-v2.js";
+export { ADVANCED_CSS, ADVANCED_RUNTIME, buildDesignCss } from "./generation2-assets.js";
+export { buildFile, compile, normalizeBase, writeBuild } from "./compiler-v3.js";
 export { AppBlocksError, formatDiagnostic, formatDiagnostics } from "./diagnostics.js";
-export { parse } from "./parser.js";
+export { parse } from "./parser-v2.js";
 export { renderNode, renderPage } from "./render.js";
 export { assertValid, validate } from "./validate.js";
 export { VERSION } from "./version.js";
